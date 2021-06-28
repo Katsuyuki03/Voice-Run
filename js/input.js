9bd7abd22a2e2a1ede47fdb7ae1563ba7e8915ef
katsuyuki03 <katsuyukid.1412@gmail.com>
2021-06-29 04:02:54 +0900

    マイクで操作をしようとした。

--- js/input.js
+++ js/input.js
@@ -1,7 +1,20 @@
+function setup() {
+    // Create an Audio input
+    mic = new p5.AudioIn();
+  
+    // start the Audio Input.
+    // By default, it does not .connect() (to the computer speakers)
+    mic.start();
+  }
+
 export class InputHandler{
+
     constructor(dino){
         document.addEventListener("keydown",event =>{
             switch(event.keyCode){
+
+
+                
                 case 38: //キーボードの上↑のボタンが押されたとき
                     dino.up();
                     break;
