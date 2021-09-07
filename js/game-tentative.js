76bb85eb6238004a5319a6e1973bdc98a4216745
katsuyuki03 <katsuyukid.1412@gmail.com>
2021-09-07 16:20:31 +0900

    画面を中央ぞろえにしようとした。（失敗）

--- js/game-tentative.js
+++ js/game-tentative.js
@@ -13,6 +13,11 @@ var score = 0;
 let canvas = document.getElementById("gameScreen");
 let ctx = canvas.getContext("2d");
 
+<div class="outer">
+<div class="inner">
+    </div>
+ </div>
+
 let backImg = document.getElementById("img_back"); //背景
 
 
