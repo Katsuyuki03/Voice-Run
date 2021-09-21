c7b7f2715ef668fcb43a21076139772b8c557709
katsuyuki03 <katsuyukid.1412@gmail.com>
2021-09-21 18:04:23 +0900

    ランダムで敵が出る

--- js/game-tentative2.js
+++ js/game-tentative2.js
@@ -90,8 +90,12 @@ function gameLoop(timestamp){
 
     counter += deltaTime;
     if(counter > interval){　//800ミリ秒から２秒間隔で爆弾を生成
-        bomb.push(new Bomb(GAME_WIDTH,GAME_HEIGHT));
-        toge.push(new Toge(GAME_WIDTH,GAME_HEIGHT));
+       if(Math.random()<0.6){
+           bomb.push(new Bomb(GAME_WIDTH,GAME_HEIGHT));
+       }else{
+            toge.push(new Toge(GAME_WIDTH,GAME_HEIGHT));
+       }
+         
         counter = 0;
         interval = getRandomInt(800,2000); //インターバルを800ミリ秒から２秒までの乱数に設定
     }
