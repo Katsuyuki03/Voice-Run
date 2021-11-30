2ffd894c98b6bc27dc7023de68a773243af2d2ab
Hiroki Kajiya <kajiya.hiroki@gmail.com>
2021-11-30 17:18:42 +0900

    no message

--- js/input2.js
+++ js/input2.js
@@ -9,41 +9,63 @@ export class InputHandler{
     //  mic.start();
    }
  
+  speech() {
+    // 音声認識バージョン
+    // 音声認識を利用するのに SpeechRecognition を利用します
+    window.SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
+    const speechRecognition = new webkitSpeechRecognition();
+
+    speechRecognition.lang = "ja-JP";
+    speechRecognition.continuous = true;
+    speechRecognition.interimResults = true;
+
+    // 音声認識が成功した時の処理を登録します
+    speechRecognition.addEventListener("result", (event) => {
+      const results = event.results;
+      for (let i = event.resultIndex; i < results.length; i++) {
+        if (results[i].isFinal) {
+          document.getElementById("result_text").innerHTML =
+            results[i][0].transcript;
+          this.speech();
+        } else {
+          document.getElementById("result_text").innerHTML =
+            results[i][0].transcript;
+          // flag_speech = 1;
+        }
+      }
+
+      const transcript = event.results[event.results.length - 1][0].transcript;
+
+      if (
+        transcript === "上。" ||
+        transcript === "うえ。" ||
+        transcript === "上" ||
+        transcript === "うえ"
+      ) {
+        this.dino.up();
+      }
+
+      if (
+        transcript === "した。" ||
+        transcript === "下。" ||
+        transcript === "した" ||
+        transcript === "下"
+      ) {
+        this.dino.down();
+      }
+    });
+
+    // 音声認識を開始する
+    speechRecognition.start();
+  }
+
      constructor(dino){
-         
        this.initilized = false;
        this.mic;
        this.vol = 0;
        this.dino = dino;
- 
-   // 音声認識バージョン
-   //   // 音声認識を利用するのに SpeechRecognition を利用します
-   window.SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
-   const speechRecognition = new webkitSpeechRecognition();
- 
-     speechRecognition.lang = "ja-JP";
-     speechRecognition.continuous = true;
-     speechRecognition.interimResults = ture;
- 
-   //   // 音声認識を開始する
-      speechRecognition.start();
- 
-   //   // 音声認識が成功した時の処理を登録します。
-    speechRecognition.addEventListener("result", (event) => {
-       const transcript = event.results[event.results.length - 1][0].transcript;
- 
-        console.log(event.results.length - 1);
- 
-       if (transcript === "上。" || transcript === "うえ。" || transcript === "上" || transcript === "うえ") {
-         dino.up();
-       }
 
-     if (transcript === "した。"　|| transcript === "下。" || transcript === "した" || transcript === "下") {
-         dino.down();
-       }
-
-     });
-     
+       this.speech();
     }
  
    setup() {
